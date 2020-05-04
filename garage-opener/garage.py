8d6ec40389d660717173ac7d8246773785efe344
Mitchell <mrjacobs55@gmail.com>
2020-05-04 13:21:43 -0400

    Garage UI and Settings

--- garage-opener/garage.py
+++ garage-opener/garage.py
@@ -2,29 +2,40 @@
 THIS FILE WILL CONTAIN ALL THE CODE TO OPEN AND CLOSE THE GARAGE
 """
 from appJar import gui
-app = gui()
+
+closed = False
+open = True
+state = closed
 
 
 def open_g():
-    print("Opening")
-    app.stop();
-    app.addLabelEntry("title", "Opening Garage Door")
-    app.setLabelBg("title", "green")
-    app.addButtons(["Dismiss"], press)
-    app.go()
+    global state
+    if state == closed:
+        app = gui()
+        print("Opening")
+        app.setFont(size=24) #, family="Sans Serif")
+        app.setTitle("Opening Garage Door")
+        app.addLabel("l1", "Opening Garage")
+        #app.addImage("opening", "closed.jpg")
+        app.setSize(400, 400)
+        app.setBg("green")
+        print("Launching")
+        app.go()
+        state = open
+    return
 
 
 def close_g():
-    print("Closing")
-    app.stop()
-    app.addLabelEntry("title", "Closing Garage Door")
-    app.setLabelBg("title", "red")
-    app.addButtons(["Dismiss"], press)
-    app.go()
-
-
-def press(button):
-    if button == "Dismiss":
-        app.stop()
-    else:
-        return
+    global state
+    if state == open:
+        app = gui()
+        print("Closing")
+        app.setFont(size=24) #, family="Sans Serif")
+        app.setTitle("Closing Garage Door")
+        app.addLabel("l1", "Closing Garage")
+        #app.addImage("closing", "closed.bmp")
+        app.setSize(400, 400)
+        app.setBg("red")
+        app.go()
+        state = closed
+    return
