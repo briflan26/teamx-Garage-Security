3a4eb09d057c5202f655fc12668c951ec8570d1f
Mitchell <mrjacobs55@gmail.com>
2020-05-04 14:40:52 -0400

    Local UI

--- garage-opener/garage.py
+++ garage-opener/garage.py
@@ -1,19 +1,27 @@
 """
 THIS FILE WILL CONTAIN ALL THE CODE TO OPEN AND CLOSE THE GARAGE
 """
+from time import sleep
+
 from appJar import gui
 
 closed = False
-open = True
+opened = True
+
 state = closed
+app = None
+
+
+
 
 
 def open_g():
     global state
+    global app
     if state == closed:
-        app = gui()
+        app = gui(showIcon=False)
         print("Opening")
-        app.setFont(size=24) #, family="Sans Serif")
+        app.setFont(size=24)
         app.setTitle("Opening Garage Door")
         app.addLabel("l1", "Opening Garage")
         app.addImage("opening", "open.gif")
@@ -21,23 +29,28 @@ def open_g():
         app.setImageSize("opening", 300, 300)
         app.setSize(400, 400)
         app.setBg("green")
+        app.setOnTop(stay=True)
         print("Launching")
         app.go()
-        state = open
+        state = opened
     return
 
 
 def close_g():
     global state
-    if state == open:
-        app = gui()
+    global app
+    if state == opened:
+        app = gui(showIcon=False)
         print("Closing")
-        app.setFont(size=24) #, family="Sans Serif")
+        app.setFont(size=24)
         app.setTitle("Closing Garage Door")
         app.addLabel("l1", "Closing Garage")
         app.addImage("closing", "closed.gif")
         app.setSize(400, 400)
         app.setBg("red")
+        app.setOnTop(stay=True)
+
+        print("Launching")
         app.go()
         state = closed
     return
